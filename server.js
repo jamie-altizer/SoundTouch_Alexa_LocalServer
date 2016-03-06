79dfd0c8a8fe5051dd9fe46476e02b44f5757e95
Zach Rose <zwrose@gmail.com>
2016-03-06 13:52:39 +0000

    dropping module approach

--- server.js
+++ server.js
@@ -10,8 +10,10 @@ var express = require('express');
 var request = require('request');
 
 var discovery = require('./discovery');
-var keymgr = require('./keymgr');
-var homemgr = require('./homemgr');
+// BasePaths should *NOT* include a trailing '/'
+var stServerBasePath = 'http://127.0.0.1:5006';
+var bridgeBasePath = 'http://alexabridge.zwrose.com';
+var bridgeID = 1;
 
 var server = this;
 
@@ -131,9 +133,172 @@ var httpServer = http.createServer(app);
 httpServer.listen(settings.port, function () {
     var port = httpServer.address().port;
     console.log('HTTP REST server listening on port', port);
-    homemgr.homemgr();
-    keymgr.keymgr();
+    updateHomeState();
+    handleKeys();
 });
 
 
 discovery.search();
+
+///////////////////////////
+function updateHomeState() {
+    // get home state from local server, and put to bridge with id
+    getBoseHomeState(function(homeStateGenerated) {
+        var postOptions = {
+            'uri': bridgeBasePath + "/api/homeStates",
+            'method': 'PUT',
+            'body': {
+                'currentState': homeStateGenerated,
+                'id': bridgeID
+            },
+            'json': true
+        };
+        request(postOptions, function (error, response, body) {
+            if (error) {
+                console.log("Error:", error)
+            } else {
+                var currentdate = new Date();
+                console.log("Updater has run. Synced: " + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getDate() + "/"
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds());
+                setTimeout(updateHomeState, 5000);
+            }
+        });
+    });
+}
+
+function getBoseHomeState(boseCallback) {
+    var homeState = {
+        speakers: {},
+        zonesPlaying: []
+    };
+
+    // go get the list of speakers from the server
+    http.get(stServerBasePath + '/device/listAdvanced', function(res) {
+        var listBody = '';
+        res.on('data', function(chunk) {listBody += chunk;});
+        res.on('end', function() {JSON.parse(listBody).forEach(function(element, index, array) {
+
+            // got get now playing info
+            http.get(stServerBasePath + '/' + encodeURIComponent(element.name) + '/nowPlaying', function(res) {
+                var nowPlayingBody = '';
+                res.on('data', function(chunk) {nowPlayingBody += chunk;});
+                res.on('end', function() {
+                    var nowPlaying = JSON.parse(nowPlayingBody).nowPlaying;
+
+                http.get(stServerBasePath + '/' + encodeURIComponent(element.name) + '/volume', function(res) {
+                    var volumeBody = '';
+                    res.on('data', function(chunk) {volumeBody += chunk;});
+                    res.on('end', function() {
+                        var volumeObj = JSON.parse(volumeBody);
+                        var currentVolume = volumeObj.volume.actualvolume;
+
+                        // go get zone info
+                        http.get(stServerBasePath + '/' + encodeURIComponent(element.name) + '/getZone', function(res) {
+                            var zoneBody = '';
+                            res.on('data', function(chunk) {zoneBody += chunk;});
+                            res.on('end', function() {
+                                var zoneInfo = JSON.parse(zoneBody).zone;
+
+                                // get the device names into the array
+                                homeState.speakers[element.name.toLowerCase()] = element;
+                                
+                                // add volume
+                                homeState.speakers[element.name.toLowerCase()].currentVolume = currentVolume;
+
+                                // check for now playing
+                                if (nowPlaying.source != 'STANDBY') {
+                                    homeState.speakers[element.name.toLowerCase()].nowPlaying = nowPlaying;
+
+                                    // check if master
+                                    if (!zoneInfo.master || zoneInfo.master == element.mac_address){
+                                        homeState.zonesPlaying.push(element.name.toLowerCase());
+                                        if(zoneInfo.master == element.mac_address) {
+                                            homeState.speakers[element.name.toLowerCase()].isMaster = true;
+                                        }
+                                    }
+                                }
+                                // ensure all speakers are discovered before proceeding
+                                if(Object.keys(homeState.speakers).length === array.length){
+                                    // console.log("Current State of the User's Home:", homeState);
+                                    boseCallback(homeState);
+                                };
+
+                            });
+                        }).on('error', function(e) {
+                            console.log("Got error: " + e.message);
+                        });
+                    });
+                }).on('error', function(e) {
+                    console.log("Got error: " + e.message);
+                });
+                });
+            }).on('error', function(e) {
+                console.log("Got error: " + e.message);
+            });
+        });
+        });
+    }).on('error', function(e) {
+        console.log("Got error: " + e.message);
+    });
+}
+function handleKeys() {
+    request({
+        'uri': bridgeBasePath + "/api/homeKeys/" + bridgeID,
+        'method': 'GET',
+        'json': true
+    }, function (error, response, body) {
+        if (error) {
+            console.log("Error:", error)
+        } else {
+            console.log(body);
+            var keyStack = body.keyStack;
+            if(keyStack.length > 0) {
+                request({
+                    'uri': stServerBasePath + keyStack[0],
+                    'method': 'GET',
+                    'json': true
+                }, function (error, response, body) {
+                    if (error) {
+                        console.log("Error:", error)
+                    } else {
+                        var currentdate = new Date(); 
+                        console.log("Called " + stServerBasePath + keyStack[0] + ". Sent: " + (currentdate.getMonth()+1)  + "/" 
+                                    + currentdate.getDate() + "/"
+                                    + currentdate.getFullYear() + " @ "  
+                                    + currentdate.getHours() + ":"  
+                                    + currentdate.getMinutes() + ":" 
+                                    + currentdate.getSeconds());
+                        
+                        // TODO need to shift on the server
+                        request({
+                            'uri': bridgeBasePath + '/api/homeKeys/shiftStack?bridgeID=' + bridgeID,
+                            'method': 'GET',
+                            'json': true
+                        }, function (error, response, body) {
+                            if (error) {
+                                console.log("Error:", error)
+                            } else {
+                                console.log("server shifted.");
+                                setTimeout(handleKeys, 1000);
+                            }
+                        });
+                    }
+                });
+                
+            } else {
+                var currentdate = new Date(); 
+                console.log("No keys found. Checked: " + (currentdate.getMonth()+1)  + "/" 
+                            + currentdate.getDate() + "/"
+                            + currentdate.getFullYear() + " @ "  
+                            + currentdate.getHours() + ":"  
+                            + currentdate.getMinutes() + ":" 
+                            + currentdate.getSeconds());
+                setTimeout(handleKeys, 1000);
+            }
+        }
+    });
+}
